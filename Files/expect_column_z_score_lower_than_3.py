8565e913266cb8a135baf1a1c8510795b2be4a70
Maximilian Huber <maximilian.huber@automators.com>
2022-11-04 13:10:33 +0100

    update

---
+++ Files/expect_column_z_score_lower_than_3.py
@@ -0,0 +1,332 @@
+"""
+This is a template for creating custom ColumnExpectations.
+For detailed instructions on how to use it, please see:
+    https://docs.greatexpectations.io/docs/guides/expectations/creating_custom_expectations/how_to_create_custom_column_aggregate_expectations
+"""
+
+import json
+from typing import Callable, Dict, Optional
+
+from numpy import array
+
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.execution_engine import (
+    PandasExecutionEngine,
+    SparkDFExecutionEngine,
+    SqlAlchemyExecutionEngine,
+)
+from great_expectations.execution_engine.execution_engine import (
+    ExecutionEngine,
+    MetricDomainTypes,
+    MetricPartialFunctionTypes,
+)
+from great_expectations.expectations.expectation import (
+    ColumnMapExpectation,
+    ExpectationValidationResult,
+)
+from great_expectations.expectations.metrics import (
+    ColumnMapMetricProvider,
+    column_condition_partial,
+    metric_partial,
+)
+from great_expectations.expectations.metrics.import_manager import F, sa
+from great_expectations.expectations.util import render_evaluation_parameter_string
+from great_expectations.render.renderer.renderer import renderer
+from great_expectations.render.types import (
+    CollapseContent,
+    RenderedStringTemplateContent,
+)
+from great_expectations.render.util import (
+    handle_strict_min_max,
+    parse_row_condition_string_pandas_engine,
+    substitute_none_for_missing,
+)
+from great_expectations.validator.metric_configuration import MetricConfiguration
+
+    
+# This class defines a Metric to support your Expectation.
+# For most ColumnMapExpectations, the main business logic for calculation will live in this class.
+class ColumnValuesLowerThanZScoreOf3(ColumnMapMetricProvider):
+
+    # This is the id string that will be used to reference your metric.
+    condition_metric_name = "column_values.lower_than_z_score_of_3"
+
+    # This method implements the core logic for the PandasExecutionEngine
+    @column_condition_partial(engine=PandasExecutionEngine)
+    def _pandas(cls, column, **kwargs):
+        return abs((abs(column.mean()) - abs(column))/column.std()) < 3
+
+
+# This class defines the Expectation itself
+class ExpectColumnZScoreLowerThan3(ColumnMapExpectation):
+    """This expectation takes the input column, calculates the standarad deviation, mean for the entire column and then calculates the 
+    Z-score for each value in the column. Any value with a Z-score larger than 3 is considered an outlier. Z-score is defined as: 
+    (value-column_mean)/standard_deviation"""
+
+    # These examples will be shown in the public gallery.
+    # They will also be executed as unit tests for your Expectation.
+    examples = [
+        {
+            "data": {"x": [1, 2, 3, 4, 5], "y": [-15, 2, 3, 4, 5]},
+            "tests": [
+                {
+                    "title": "basic_positive_test",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "x",
+                    },
+                    "out": {"success": True},
+                },
+                {
+                    "title": "basic_negative_test",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "y",
+                    },
+                    "out": {"success": False},
+                },
+            ],
+            "test_backends": [
+                {
+                    "backend": "pandas",
+                    "dialects": None,
+                },
+                # {
+                #     "backend": "sqlalchemy",
+                #     "dialects": ["sqlite", "postgresql"],
+                # },
+                # {
+                #     "backend": "spark",
+                #     "dialects": None,
+                # },
+            ],
+        }
+    ]
+
+    # This is the id string of the Metric used by this Expectation.
+    # For most Expectations, it will be the same as the `condition_metric_name` defined in your Metric class above.
+    map_metric = "column_values.lower_than_z_score_of_3"
+
+    # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
+    # Please see https://docs.greatexpectations.io/en/latest/reference/core_concepts/expectations/expectations.html#expectation-concepts-domain-and-success-keys
+    # for more information about domain and success keys, and other arguments to Expectations
+    success_keys = ("mostly",)
+
+    # This dictionary contains default values for any parameters that should have default values
+    default_kwarg_values = {}
+
+    @renderer(renderer_type="renderer.diagnostic.observed_value")
+    @render_evaluation_parameter_string
+    def _diagnostic_observed_value_renderer(
+        cls,
+        configuration: ExpectationConfiguration = None,
+        result: ExpectationValidationResult = None,
+        language: str = None,
+        runtime_configuration: dict = None,
+        **kwargs,
+    ):
+        assert result, "Must provide a result object."
+
+        result_dict = result.result
+        if result_dict is None:
+            return "--"
+
+        if result_dict.get("observed_value"):
+            observed_value = result_dict.get("observed_value")
+            if isinstance(observed_value, (int, float)) and not isinstance(
+                observed_value, bool
+            ):
+                return num_to_str(observed_value, precision=10, use_locale=True)
+            return str(observed_value)
+        elif result_dict.get("unexpected_percent") is not None:
+            return (
+                num_to_str(result_dict.get("unexpected_percent"), precision=5)
+                + "% unexpected"
+            )
+        else:
+            return "--"
+
+    @renderer(renderer_type="renderer.diagnostic.unexpected_statement")
+    @render_evaluation_parameter_string
+    def _diagnostic_unexpected_statement_renderer(
+        cls,
+        configuration: ExpectationConfiguration = None,
+        result: ExpectationValidationResult = None,
+        language: str = None,
+        runtime_configuration: dict = None,
+        **kwargs,
+    ):
+        assert result, "Must provide a result object."
+
+        success = result.success
+        result = result.result
+
+        if result.exception_info["raised_exception"]:
+            exception_message_template_str = (
+                "\n\n$expectation_type raised an exception:\n$exception_message"
+            )
+
+            exception_message = RenderedStringTemplateContent(
+                **{
+                    "content_block_type": "string_template",
+                    "string_template": {
+                        "template": exception_message_template_str,
+                        "params": {
+                            "expectation_type": result.expectation_config.expectation_type,
+                            "exception_message": result.exception_info[
+                                "exception_message"
+                            ],
+                        },
+                        "tag": "strong",
+                        "styling": {
+                            "classes": ["text-danger"],
+                            "params": {
+                                "exception_message": {"tag": "code"},
+                                "expectation_type": {
+                                    "classes": ["badge", "badge-danger", "mb-2"]
+                                },
+                            },
+                        },
+                    },
+                }
+            )
+
+            exception_traceback_collapse = CollapseContent(
+                **{
+                    "collapse_toggle_link": "Show exception traceback...",
+                    "collapse": [
+                        RenderedStringTemplateContent(
+                            **{
+                                "content_block_type": "string_template",
+                                "string_template": {
+                                    "template": result.exception_info[
+                                        "exception_traceback"
+                                    ],
+                                    "tag": "code",
+                                },
+                            }
+                        )
+                    ],
+                }
+            )
+
+            return [exception_message, exception_traceback_collapse]
+
+        if success or not result_dict.get("unexpected_count"):
+            return []
+        else:
+            unexpected_count = num_to_str(
+                result_dict["unexpected_count"], use_locale=True, precision=20
+            )
+            unexpected_percent = (
+                num_to_str(result_dict["unexpected_percent"], precision=4) + "%"
+            )
+            element_count = num_to_str(
+                result_dict["element_count"], use_locale=True, precision=20
+            )
+
+            template_str = (
+                "\n\n$unexpected_count unexpected values found. "
+                "$unexpected_percent of $element_count total rows."
+            )
+
+            return [
+                RenderedStringTemplateContent(
+                    **{
+                        "content_block_type": "string_template",
+                        "string_template": {
+                            "template": template_str,
+                            "params": {
+                                "unexpected_count": unexpected_count,
+                                "unexpected_percent": unexpected_percent,
+                                "element_count": element_count,
+                            },
+                            "tag": "strong",
+                            "styling": {"classes": ["text-danger"]},
+                        },
+                    }
+                )
+            ]
+
+    @renderer(renderer_type="renderer.diagnostic.unexpected_table")
+    @render_evaluation_parameter_string
+    def _diagnostic_unexpected_table_renderer(
+        cls,
+        configuration: ExpectationConfiguration = None,
+        result: ExpectationValidationResult = None,
+        language: str = None,
+        runtime_configuration: dict = None,
+        **kwargs,
+    ):
+        try:
+            result_dict = result.result
+        except KeyError:
+            return None
+
+        if result_dict is None:
+            return None
+
+        if not result_dict.get("partial_unexpected_list") and not result_dict.get(
+            "partial_unexpected_counts"
+        ):
+            return None
+
+        table_rows = []
+
+        if result_dict.get("partial_unexpected_counts"):
+            total_count = 0
+            for unexpected_count_dict in result_dict.get("partial_unexpected_counts"):
+                value = unexpected_count_dict.get("value")
+                count = unexpected_count_dict.get("count")
+                total_count += count
+                if value is not None and value != "":
+                    table_rows.append([value, count])
+                elif value == "":
+                    table_rows.append(["EMPTY", count])
+                else:
+                    table_rows.append(["null", count])
+
+            if total_count == result_dict.get("unexpected_count"):
+                header_row = ["Unexpected Value", "Count"]
+            else:
+                header_row = ["Sampled Unexpected Values"]
+                table_rows = [[row[0]] for row in table_rows]
+
+        else:
+            header_row = ["Sampled Unexpected Values"]
+            sampled_values_set = set()
+            for unexpected_value in result_dict.get("partial_unexpected_list"):
+                if unexpected_value:
+                    string_unexpected_value = str(unexpected_value)
+                elif unexpected_value == "":
+                    string_unexpected_value = "EMPTY"
+                else:
+                    string_unexpected_value = "null"
+                if string_unexpected_value not in sampled_values_set:
+                    table_rows.append([unexpected_value])
+                    sampled_values_set.add(string_unexpected_value)
+
+        unexpected_table_content_block = RenderedTableContent(
+            **{
+                "content_block_type": "table",
+                "table": table_rows,
+                "header_row": header_row,
+                "styling": {
+                    "body": {"classes": ["table-bordered", "table-sm", "mt-3"]}
+                },
+            }
+        )
+
+        return unexpected_table_content_block
+
+    # This dictionary contains metadata for display in the public gallery
+    library_metadata = {
+        "tags": [],
+        "contributors": ["@NatanMish"],
+    }
+
+
+if __name__ == "__main__":
+    ExpectColumnZScoreLowerThan3().print_diagnostic_checklist()
